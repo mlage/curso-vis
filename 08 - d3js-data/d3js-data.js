c87d91848b70188324c3ad0f68cbd63524241be9
Marcos Lage <mlage@ic.uff.br>
2020-08-07 10:39:50 -0300

    Renaming folders.

---
+++ 08 - d3js-data/d3js-data.js
@@ -0,0 +1,121 @@
+class D3jsData {
+  constructor() {
+    this.circles = [];
+    this.squares = [];
+
+    this.createSvg();
+  }
+
+  createSvg() {
+    var node = d3.select("#main")
+      .append("svg")
+      .attr('x', 10)
+      .attr('y', 10)
+      .attr('width', 800)
+      .attr('height', 600);
+    
+    return node;
+  }
+
+  createCircles(svg) {
+    const t = svg.transition()
+        .duration(1500);
+
+    let circles = svg.selectAll('circle')
+      .data(this.circles);
+
+    circles.enter()
+      .append('circle')
+        .attr('cx', d => d.cx)
+        .attr('cy', d => d.cy)
+        .attr('r' , d => d.r)
+        .style('fill', 'RoyalBlue');
+
+    circles
+      .attr('cx', d => d.cx)
+      .attr('cy', d => d.cy)
+      .attr('r' , d => d.r)
+      .style('fill', 'SeaGreen');
+
+    circles.exit()
+      .style('fill', 'IndianRed')
+      .call(
+        ex => ex.transition(t)
+        .remove()
+      )
+  }
+
+  createSquares(svg) {
+    const t = svg.transition()
+        .duration(1500);
+
+    svg.selectAll('rect')
+      .data(this.squares)
+      .join(
+        // enter
+        en => en.append('rect')
+        .style('fill', 'RoyalBlue'),
+        
+        // update
+        up => up.style('fill', 'SeaGreen'),
+        
+        // exit
+        ex => ex.style("fill", "IndianRed")
+        .call(ex => ex.transition(t)
+          .remove()
+        )
+      )
+      .attr('x', d => d.x)
+      .attr('y', d => d.y)
+      .attr('width' , d => d.l)
+      .attr('height', d => d.l)
+  }
+
+  getRandomColor() {
+    return 'rgb(' + Math.floor(Math.random()*256) + ',' + Math.floor(Math.random()*256) + ',' + Math.floor(Math.random()*256) + ')';
+  }
+
+  run(circles, squares) {
+    this.circles = circles;
+    this.squares = squares;
+
+    var svg = d3.select("svg")    
+    
+    this.createCircles(svg);
+    this.createSquares(svg);
+  }
+}
+
+window.onload = () => {
+  let app = new D3jsData();
+  
+  setInterval(() => { 
+    let nc = Math.floor(Math.random() * 50);
+    let ns = Math.floor(Math.random() * 50);
+    
+    const circles = []
+    for (let c=0; c<nc; c++) {
+      const circle = {
+        cx: Math.random()*800,
+        cy: Math.random()*600,
+        r : Math.random()*20 + 10
+      }
+      circles.push(circle);
+    }
+
+    const squares = []
+    for (let s=0; s<ns; s++) {
+      const square = {
+        x: Math.random()*800,
+        y: Math.random()*600,
+        l: Math.random()*20 + 10
+      }
+      squares.push(square);
+    }
+
+    app.run(circles, squares);
+    
+    console.log('chart updated...')
+  }, 3000);
+  
+}
