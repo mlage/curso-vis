30374b03e2cc71e31b9dc8fe90e0f43d217c579a
Marcos Lage <mlage@ic.uff.br>
2024-04-24 13:06:58 -0300

    Exemplo 11 - Observable Framework.

--- 11 - observable/docs/data/02-dados.csv.py
+++ 11 - observable/docs/data/02-dados.csv.py
@@ -2,7 +2,7 @@ import sys, os
 import pandas as pd
 
 real_path = os.path.realpath(__file__) .split("/")
-real_path[-2] = "datasets"
+real_path[-2] = "data"
 real_path[-1] = "superstore.csv"
 
 real_path = "/".join(real_path);
